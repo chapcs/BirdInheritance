f2beafc9c66036f9caa85ab7bfc805ed3bf05cc8
chapcs <benchapman003@gmail.com>
2023-04-23 10:00:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Bird bird;
+        }
+    }
+}
+
+class Egg
+{
+    public double Size { get; private set; }
+    public string Color { get; private set; }
+    public Egg(double size, string color)
+    {
+        Size = size;
+        Color = color;
+    }
+    public string Description
+    {
+        get { return $"A {Size:0.0}cm {Color} egg"; }
+    }
+}
+
+class Bird
+{
+
+}
+
+class Pigeon : Bird
+{
+
+}
+
+class Ostroch : Bird
+{
+
+}
