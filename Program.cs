0267bc1f564a1cd68c67ed3c2748acdcbe5e2df1
chapcs <benchapman003@gmail.com>
2023-04-23 10:08:48 -0400

    Added Bird base class and subclasses

--- Program.cs
+++ Program.cs
@@ -26,15 +26,36 @@ class Egg
 
 class Bird
 {
-
+    public static Random Randomizer = new Random();
+    public virtual Egg[] LayEggs(int numberOfEggs)
+    {
+        Console.Error.WriteLine("Bird.LayEggs should never get called");
+        return new Egg[0];
+    }
 }
 
 class Pigeon : Bird
 {
-
+    public override Egg[] LayEggs(int numberOfEggs)
+    {
+        Egg[] eggs = new Egg[numberOfEggs];
+        for (int i = 0; i < numberOfEggs; i++)
+        {
+            eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+        }
+        return eggs;
+    }
 }
 
-class Ostroch : Bird
+class Ostrich : Bird
 {
-
+    public override Egg[] LayEggs(int numberOfEggs)
+    {
+        Egg[] eggs = new Egg[numberOfEggs];
+        for (int i = 0; i < numberOfEggs; i++)
+        {
+            eggs[i] = new Egg(Bird.Randomizer.NextDouble() + 12, "speckled");
+        }
+        return eggs;
+    }
 }
