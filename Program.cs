6c4cf4285918e656adacba910b8255e93f06cbc5
chapcs <benchapman003@gmail.com>
2023-04-29 08:37:36 -0400

    Added feature for brokenegg

--- Program.cs
+++ Program.cs
@@ -43,7 +43,19 @@ class Egg
     }
     public string Description
     {
-        get { return $"A {Size:0.0}cm {Color} egg"; }
+        get 
+        {
+            if (Size == 0) return $"A {Color} egg";
+            return $"A {Size:0.0}cm {Color} egg"; 
+        }
+    }
+}
+
+class BrokenEgg : Egg
+{
+    public BrokenEgg(string color) : base(0, $"broken {color}")
+    {
+        Console.WriteLine("A bird has laid a broken egg");
     }
 }
 
@@ -64,7 +76,10 @@ class Pigeon : Bird
         Egg[] eggs = new Egg[numberOfEggs];
         for (int i = 0; i < numberOfEggs; i++)
         {
-            eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+            if (Bird.Randomizer.Next(4) == 0)
+                eggs[i] = new BrokenEgg("white"); //modified to only take the color param
+            else
+                eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
         }
         return eggs;
     }
